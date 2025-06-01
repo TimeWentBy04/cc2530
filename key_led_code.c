5c2502e3da1784f8bc4e1b08f3ce70d5214790e7
TimeWentBy04 <144317664+TimeWentBy04@users.noreply.github.com>
2025-06-01 20:11:28 +0800

    Update and rename code.c to key_led_code.c

--- key_led_code.c
+++ key_led_code.c
@@ -1,9 +1,9 @@
 #include "ioCC2530.h"
 
 #define LED1 P1_0 
-#define SW1 P1_2
+#define SW1 P1_2 
 
-void delay(unsigned int time){
+void delay(unsigned  int time){
   unsigned int i;
   unsigned char j;
   for(i=0;i<time;i++)
@@ -15,27 +15,20 @@ void delay(unsigned int time){
 }
 
 void main(void){
-  int a =0;
   P1SEL&=~0x05;
   P1DIR|=0x01;
   P1DIR&=~0x04;
   
+  P1INP&=~0x04;
+  P2INP&=~0x40;
   LED1=0;
   while(1){
+  if(SW1==0){
+    delay(100);
     if(SW1==0){
-      delay(20);
-      if(SW1==0){
-        a=a==1?0:1;
-        while(!SW1);
-      }
+      LED1 = ~LED1;
+      while(!SW1);
     }
-        if(a==1){
-         LED1=~LED1;
-         delay(300);
-        }
-        if(a==0){
-         LED1=0;
-         delay(300);
-        }
+  }
   }
 }
